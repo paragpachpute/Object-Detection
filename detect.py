b4d903e6bf55ba7060966fc5f054719fe9332e65
Parag Pachpute <paragpachpute3@gmail.com>
2020-05-26 12:31:34 -0400

    Object detection using OpenCV

--- detect.py
+++ detect.py
@@ -4,10 +4,32 @@ import cv2
 import matplotlib.pyplot as plt
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
+import numpy as np
 
 im = cv2.imread('images/img.jpeg')
+plt.figure()
 bbox, label, conf = cv.detect_common_objects(im)
-
 output_image = draw_bbox(im, bbox, label, conf)
-plt.imshow(output_image)
+plt.imshow(cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+im = cv2.imread('images/1.jpeg')
+plt.figure()
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+plt.imshow(cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+im = cv2.imread('images/2.jpeg')
+plt.figure()
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+plt.imshow(cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB))
+plt.show()
+
+im = cv2.imread('images/3.jpeg')
+plt.figure()
+bbox, label, conf = cv.detect_common_objects(im)
+output_image = draw_bbox(im, bbox, label, conf)
+plt.imshow(cv2.cvtColor(output_image, cv2.COLOR_BGR2RGB))
 plt.show()
